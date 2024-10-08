d075a0d0ed9f06475895ae469fa575c5b078004d
harryle <lehoangsonsg@gmail.com>
2024-10-08 19:38:57 +1030

    WIP: create item from cfg

--- stac_generator/base/generator.py
+++ stac_generator/base/generator.py
@@ -1,3 +1,4 @@
+import abc
 from typing import Any, Generic, cast
 
 import httpx
@@ -14,36 +15,41 @@ class STACGenerator(Generic[T]):
     source_type: type[T]
 
     @classmethod
-    def __class_getitem__(cls, model_type: type) -> type:
-        cls.source_type = model_type
+    def __class_getitem__(cls, source_type: type) -> type:
+        cls.source_type = source_type
         return cls
 
     def __init__(
         self,
-        stac_cfg: STACConfig,
         source_df: DataFrame[DataFrameSchema[T]],
         driver: IODriver,
+        catalog_cfg: STACConfig | None = None,
+        collection_cfg: STACConfig | None = None,
     ) -> None:
-        self.stac_cfg = stac_cfg
+        self.catalog_cfg = catalog_cfg
+        self.collection_cfg = collection_cfg
         self.source_df = source_df
         self.driver = driver
 
-    def create_item(self, source_cfg: T) -> list[pystac.Item]:
-        return NotImplemented
+    @abc.abstractmethod
+    def create_item_from_config(self, source_cfg: T) -> list[pystac.Item]:
+        raise NotImplementedError
 
-    def extract_cfg(self) -> list[T]:
-        return [
-            self.source_type(**cast(dict[str, Any], self.source_df.loc[i, :].to_dict()))
-            for i in range(len(self.source_df))
-        ]
+    def _extract_cfg(self) -> list[T]:
+        return [self.source_type(**cast(dict[str, Any], self.source_df.loc[i, :].to_dict())) for i in range(len(self.source_df))]
 
     @property
     def providers(self) -> list[pystac.Provider] | None:
-        if self.stac_cfg.providers:
-            return [
-                pystac.Provider.from_dict(item.model_dump()) for item in self.stac_cfg.providers
-            ]
+        if self.collection_cfg and self.collection_cfg.providers:
+            return [pystac.Provider.from_dict(item.model_dump()) for item in self.collection_cfg.providers]
         return None
+
+    def create_items(self) -> list[pystac.Item]:
+        configs = self._extract_cfg()
+        items = []
+        for config in configs:
+            items.extend(self.create_item_from_config(config))
+        return items
 
     def create_collection(
         self,
@@ -51,26 +57,35 @@ class STACGenerator(Generic[T]):
         extent: Extent,
         asset: pystac.Asset,
     ) -> pystac.Collection:
+        if self.collection_cfg is None:
+            raise ValueError("Generating collection requires non null collection config")
         collection = pystac.Collection(
-            id=self.stac_cfg.id,
-            description=self.stac_cfg.description
-            if self.stac_cfg.description
-            else f"Auto-generated collection {self.stac_cfg.id} with stac_generator",
+            id=self.collection_cfg.id,
+            description=(
+                self.collection_cfg.description
+                if self.collection_cfg.description
+                else f"Auto-generated collection {self.collection_cfg.id} with stac_generator"
+            ),
             extent=extent,
-            title=self.stac_cfg.title,
-            license=self.stac_cfg.license if self.stac_cfg.license else "proprietory",
+            title=self.collection_cfg.title,
+            license=self.collection_cfg.license if self.collection_cfg.license else "proprietory",
             providers=self.providers,
             assets={"source": asset},
         )
         collection.add_items(items)
         return collection
 
+    def create_catalog(self, collection: pystac.Collection) -> pystac.Catalog:
+        if self.catalog_cfg is None:
+            raise ValueError("Generating catalog requires non null catalog config")
+        catalog = pystac.Catalog(id=self.catalog_cfg.id, description=self.catalog_cfg.description, title=self.catalog_cfg.title)
+        catalog.add_child(collection)
+        return catalog
+
 
 class STACLoader:
     @staticmethod
-    def _load_from_file(
-        entity: STACEntityT, location: str
-    ) -> pystac.Catalog | pystac.Collection | pystac.Item | pystac.ItemCollection:
+    def _load_from_file(entity: STACEntityT, location: str) -> pystac.Catalog | pystac.Collection | pystac.Item | pystac.ItemCollection:
         match entity:
             case "Item":
                 return pystac.Item.from_file(location)
@@ -84,9 +99,7 @@ class STACLoader:
                 raise ValueError(f"Invalid STAC type: {entity}")
 
     @staticmethod
-    def _load_from_api(
-        entity: STACEntityT, api_endpoint: str
-    ) -> pystac.Catalog | pystac.Collection | pystac.Item | pystac.ItemCollection:
+    def _load_from_api(entity: STACEntityT, api_endpoint: str) -> pystac.Catalog | pystac.Collection | pystac.Item | pystac.ItemCollection:
         response = httpx.get(api_endpoint)
         json_data = response.json()
         match entity:
