a3bc27e85ffc8a1d41b4940f3ab2b467fb5552f4
hoangson <lehoangsonsg@gmail.com>
2024-11-19 15:35:29 +1030

    feat: disable logging from other modules

--- stac_generator/__init__.py
+++ stac_generator/__init__.py
@@ -6,3 +6,6 @@ __all__ = ("StacGeneratorFactory",)
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
+
+for _ in logging.root.manager.loggerDict:
+    logging.getLogger(_).disabled = True
