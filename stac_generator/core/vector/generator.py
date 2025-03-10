b878b51b826eff45a115699e0b0eca81145335bd
hoangson <lehoangsonsg@gmail.com>
2025-03-10 21:06:11 +1030

    feat: update datetime columns

--- stac_generator/core/vector/generator.py
+++ stac_generator/core/vector/generator.py
@@ -49,18 +49,14 @@ class VectorGenerator(BaseVectorGenerator[VectorConfig]):
             try:
                 raw_df = gpd.read_file(source_cfg["location"])
             except Exception as e:
-                raise ValueError(
-                    "Compressed zip contains multiple vector layers. Please specify a layer in the original config"
-                ) from e
+                raise ValueError("Compressed zip contains multiple vector layers. Please specify a layer in the original config") from e
         else:
             raw_df = gpd.read_file(source_cfg["location"], layer=getattr(source_cfg, "layer", None))
         columns = []
         for name in raw_df.columns:
             if name != "geometry":
                 columns.append(ColumnInfo(name=name, description=f"{name}_description"))
-        return VectorConfig(**source_cfg, column_info=columns).model_dump(
-            mode="json", exclude_none=True
-        )
+        return VectorConfig(**source_cfg, column_info=columns).model_dump(mode="json", exclude_none=True)
 
     def create_item_from_config(self, source_cfg: VectorConfig) -> pystac.Item:
         """Create item from vector config
@@ -74,9 +70,7 @@ class VectorGenerator(BaseVectorGenerator[VectorConfig]):
         assets = {
             "data": pystac.Asset(
                 href=str(source_cfg.location),
-                media_type=pystac.MediaType.GEOJSON
-                if source_cfg.location.endswith(".geojson")
-                else "application/x-shapefile",
+                media_type=pystac.MediaType.GEOJSON if source_cfg.location.endswith(".geojson") else "application/x-shapefile",
                 roles=["data"],
                 description="Raw vector data",
             )
@@ -85,9 +79,7 @@ class VectorGenerator(BaseVectorGenerator[VectorConfig]):
 
         # Only read relevant fields
         if isinstance(source_cfg.column_info, list):
-            columns = [
-                col["name"] if isinstance(col, dict) else col for col in source_cfg.column_info
-            ]
+            columns = [col["name"] if isinstance(col, dict) else col for col in source_cfg.column_info]
         else:
             columns = None
         raw_df = gpd.read_file(source_cfg.location, columns=columns, layer=source_cfg.layer)
@@ -97,11 +89,10 @@ class VectorGenerator(BaseVectorGenerator[VectorConfig]):
         # Only compare if epsg is provided at config
         if source_cfg.epsg is not None:
             if reliable and epsg != source_cfg.epsg:
-                raise ValueError(
-                    f"Source crs: {raw_df.crs} does not match config epsg: {source_cfg.epsg}"
-                )
+                raise ValueError(f"Source crs: {raw_df.crs} does not match config epsg: {source_cfg.epsg}")
             epsg = source_cfg.epsg
 
+        start_datetime, end_datetime = None, None
         # Read join file
         if source_cfg.join_file:
             if source_cfg.join_attribute_vector not in raw_df:
@@ -109,13 +100,19 @@ class VectorGenerator(BaseVectorGenerator[VectorConfig]):
                     f"If a join file is provided, expects join attribute vector: {source_cfg.join_attribute_vector} to be a valid attribute of the vector file."
                 )
             # Try reading join file and raise errors if columns not provided
-            _read_csv(
-                src_path=source_cfg.join_file,
-                required=[cast(str, source_cfg.join_field)],
-                date_format=source_cfg.date_format,
-                date_col=source_cfg.join_T_column,
-                columns=source_cfg.join_column_info,
-            )
+            try:
+                raw_df = _read_csv(
+                    src_path=source_cfg.join_file,
+                    required=[cast(str, source_cfg.join_field)],
+                    date_format=source_cfg.date_format,
+                    date_col=source_cfg.join_T_column,
+                    columns=source_cfg.join_column_info,
+                )
+            except ValueError as e:
+                raise ValueError(f"Join file associated with vector file: {source_cfg.id} may not have the specified column") from e
+            if source_cfg.join_T_column:
+                start_datetime = raw_df[source_cfg.join_T_column].min()
+                end_datetime = raw_df[source_cfg.join_T_column].max()
 
         # Make properties
         properties = source_cfg.model_dump(
@@ -135,4 +132,12 @@ class VectorGenerator(BaseVectorGenerator[VectorConfig]):
             exclude_none=True,
         )
 
-        return self.df_to_item(raw_df, assets, source_cfg, properties, epsg)
+        return self.df_to_item(
+            raw_df,
+            assets,
+            source_cfg,
+            properties,
+            epsg,
+            start_datetime,
+            end_datetime,
+        )
